3340fc13c270acad3e3d8f4c99eb7bcaf9dfdb32
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 14:04:04 +0100

    Agregar enlaces de navegación en el componente Footer y aplicar estilos personalizados; crear archivo CSS para el Footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,22 +1,29 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
         <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item">
-                    <a href="#" className="nav-link px-2 text-body-secondary">Home</a>
+                <Link to="/homepage" className={`nav-link px-2 text-body-secondary ${styles.navLink}`}>
+                        Home
+                    </Link>                    
                     </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link px-2 text-body-secondary">Landing</a>
-                    </li>
+                <Link to="/" className={`nav-link px-2 text-body-secondary ${styles.navLink}`}>
+                        Landing
+                    </Link>                       </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link px-2 text-body-secondary">Sign Up </a>
-                    </li>
+                <Link to="/login" className={`nav-link px-2 text-body-secondary ${styles.navLink}`}>
+                        Sign Up
+                    </Link>                       </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link px-2 text-body-secondary">Detalles</a>
-                    </li>
+                <Link to="/details" className={`nav-link px-2 text-body-secondary ${styles.navLink}`}>
+                        Detalles
+                    </Link>                       </li>
             </ul>
             <p className="text-center text-body-secondary"> 2025 Esteban Fernández</p>
         </footer>
