33951c2cebebd80dceb7244635bbb5fc146f100b
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 13:21:00 +0100

    Agregar componente Footer con enlaces de navegación y mensaje de copyright; actualizar Landing para cumplir con las normas de eslint.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item">
+                    <a href="#" className="nav-link px-2 text-body-secondary">Home</a>
+                    </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link px-2 text-body-secondary">Landing</a>
+                    </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link px-2 text-body-secondary">Sign Up </a>
+                    </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link px-2 text-body-secondary">Detalles</a>
+                    </li>
+            </ul>
+            <p className="text-center text-body-secondary"> 2025 Esteban Fernández</p>
+        </footer>
+    )
 }
 
 export default Footer
