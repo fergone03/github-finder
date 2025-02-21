6af19f1a70f9a77cf0482bc92051e9f046964d32
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 13:12:57 +0100

    Agregar componente Footer y actualizar App para incluirlo en la estructura

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import LoginPage from "./pages/LoginPage"; // Crea esta página
-import HomePage from "./pages/HomePage";   // Mueve tu código principal aquí
+import LoginPage from "./pages/LoginPage"; 
+import HomePage from "./pages/HomePage";  
 import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,8 +13,8 @@ const App = () => {
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Landing />} />
-
       </Routes>
+      <Footer />
     </Router>
   );
 };
