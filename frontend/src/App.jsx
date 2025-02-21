7b2271e42609ece4306d9cf4f54143cee648cff3
fergone03 <114108805+fergone03@users.noreply.github.com>
2025-02-21 13:10:38 +0100

    Agregar componente Landing y configurar ruta principal en App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import LoginPage from "./pages/LoginPage"; // Crea esta página
 import HomePage from "./pages/HomePage";   // Mueve tu código principal aquí
+import Landing from "./components/Landing";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Landing />} />
+
       </Routes>
     </Router>
   );
